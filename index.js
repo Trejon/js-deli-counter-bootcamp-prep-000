727c2bf2f52c47b7b8a0b492875d93e715757545
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-24 14:57:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(x) {
     for (var i = 0; i < x.length; i++) {
   newLine += (i + 1) + "." + x[i] + ",";
     }
-    line = line.slice(0, line.length-2);
+    newLine = line.slice(0, line.length-2);
     return "The line is currently: " + line;
   }
 }
