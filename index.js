e7108f81facfd62ca1107916de8aa9b7b7b1ef55
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-24 14:55:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ function nowServing(x) {
  }
 }
 
-function currentLine(katzDeliLine) {
-  var str = "The line is currently:";
+function currentLine(x) {
   var  newLine = []; 
-  if (katzDeliLine.length === 0) return "The line is currently empty";
-  else {
-    for (var i = 0; i < katzDeliLine.length; i++) {
-      newLine[i] = `${[i + 1]}. ${katzDeliLine[i]}`;
-      }
-      str += newLine.join ('.'); 
-      return str;
+  if (x.length === 0) {
+    return "The line is currently empty.";
+  }else {
+    for (var i = 0; i < x.length; i++) {
+  newLine += (i + 1) + "." + x[i] + ",";
+    }
+    line = line.slice(0, line.length-2);
+    return "The line is currently: " + line;
   }
 }
