8f5caddc416fd83041b2a04d8b3b92ff8abd9276
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-24 14:14:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function takeANumber(katzDeliLine,name) {
+katzDeliLine.push(name);
+return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+ if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift(name)}. `;
+  else return "There is nobody waiting to be served!";
+}
+
+function currentLine(katzDeliLine) {
+  var str = "The line is currently:";
+  var 
+}
